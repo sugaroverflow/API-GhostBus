d260e709104700a79e5f7edb74b0a1acc830dabe
SugarOverflow <sugaroverflow@gmail.com>
2015-04-18 01:07:43 -0400

    parsed for lat/long values

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import json
 response = urllib2.urlopen('http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=9030507a-62c2-4168-825d-ddf477ec1b22&OperatorRef=MTA+NYCT&LineRef=B54&VehicleRef=4105')
 html = response.read()
 h = json.loads(html)
-print h['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']['MonitoredVehicleJourney'][0]['VehicleLocation']
+print h['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][0]['MonitoredVehicleJourney']['VehicleLocation']
