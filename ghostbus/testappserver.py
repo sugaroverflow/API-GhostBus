2ebe2297b088e219d25f2baa03fb99afe8fb3490
SugarOverflow <sugaroverflow@gmail.com>
2015-05-01 13:29:29 -0400

    edited test server for api

--- ghostbus/testappserver.py
+++ ghostbus/testappserver.py
@@ -4,6 +4,11 @@ import testapp
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/.')
+def main_page():
+    return "This is the main page. Possible commands: \n \
+    /alldata or /data/bus# or /gps/bus#"
+
 @app.route('/alldata')
 def _all_data():
     return testapp._get_all_data()
